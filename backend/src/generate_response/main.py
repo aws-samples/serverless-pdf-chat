c4a8d2bf97e68408e5d2013a29eceec08e57068a
kbuilder <kbuilder@amazon.com>
2024-06-03 10:52:21 -0700

    Add Claude v3

--- backend/src/generate_response/main.py
+++ backend/src/generate_response/main.py
@@ -1,7 +1,9 @@
-import os, json
+import os
+import json
 import boto3
 from aws_lambda_powertools import Logger
 from langchain.llms.bedrock import Bedrock
+from langchain_community.chat_models import BedrockChat
 from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
 from langchain.memory import ConversationBufferMemory
 from langchain.embeddings import BedrockEmbeddings
@@ -11,38 +13,32 @@ from langchain.chains import ConversationalRetrievalChain
 
 MEMORY_TABLE = os.environ["MEMORY_TABLE"]
 BUCKET = os.environ["BUCKET"]
-
+MODEL_ID = os.environ["MODEL_ID"]
 
 s3 = boto3.client("s3")
 logger = Logger()
 
 
-@logger.inject_lambda_context(log_event=True)
-def lambda_handler(event, context):
-    event_body = json.loads(event["body"])
-    file_name = event_body["fileName"]
-    human_input = event_body["prompt"]
-    conversation_id = event["pathParameters"]["conversationid"]
-
-    user = event["requestContext"]["authorizer"]["claims"]["sub"]
-
-    s3.download_file(BUCKET, f"{user}/{file_name}/index.faiss", "/tmp/index.faiss")
-    s3.download_file(BUCKET, f"{user}/{file_name}/index.pkl", "/tmp/index.pkl")
-
+def get_embeddings():
     bedrock_runtime = boto3.client(
         service_name="bedrock-runtime",
         region_name="us-east-1",
     )
 
-    embeddings, llm = BedrockEmbeddings(
+    embeddings = BedrockEmbeddings(
         model_id="amazon.titan-embed-text-v1",
         client=bedrock_runtime,
         region_name="us-east-1",
-    ), Bedrock(
-        model_id="anthropic.claude-v2", client=bedrock_runtime, region_name="us-east-1"
     )
-    faiss_index = FAISS.load_local("/tmp", embeddings)
+    return embeddings
 
+def get_faiss_index(embeddings, user, file_name):
+    s3.download_file(BUCKET, f"{user}/{file_name}/index.faiss", "/tmp/index.faiss")
+    s3.download_file(BUCKET, f"{user}/{file_name}/index.pkl", "/tmp/index.pkl")
+    faiss_index = FAISS.load_local("/tmp", embeddings, allow_dangerous_deserialization=True)
+    return faiss_index
+
+def create_memory(conversation_id):
     message_history = DynamoDBChatMessageHistory(
         table_name=MEMORY_TABLE, session_id=conversation_id
     )
@@ -54,7 +50,34 @@ def lambda_handler(event, context):
         output_key="answer",
         return_messages=True,
     )
+    return memory
 
+def handle_claude_v3(faiss_index, memory, human_input):
+
+    chat = BedrockChat(
+        model_id=MODEL_ID,
+        model_kwargs={'temperature': 0.0}
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=chat,
+        chain_type="stuff",
+        retriever=faiss_index.as_retriever(),
+        memory=memory,
+        return_source_documents=True,
+    )
+
+    response = chain.invoke({"question": human_input})
+
+    print(f"{MODEL_ID} response: {response}")
+    return response
+
+def handle_claude_v2(faiss_index, memory, human_input, bedrock_runtime):
+
+    llm = Bedrock(
+        model_id=MODEL_ID, client=bedrock_runtime, region_name="us-east-1"
+    )
+    
     qa = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=faiss_index.as_retriever(),
@@ -62,9 +85,41 @@ def lambda_handler(event, context):
         return_source_documents=True,
     )
 
-    res = qa({"question": human_input})
+    response = qa({"question": human_input})
 
-    logger.info(res)
+    print(f"{MODEL_ID} response: {response}")
+    return response
+
+model_handlers = {
+    "anthropic.claude-3-sonnet-20240229-v1:0": handle_claude_v3,
+    "anthropic.claude-3-haiku-20240307-v1:0": handle_claude_v3,
+    "anthropic.claude-v2": handle_claude_v2,
+    "anthropic.claude-v2:1": handle_claude_v2,
+}
+
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    event_body = json.loads(event["body"])
+    file_name = event_body["fileName"]
+    human_input = event_body["prompt"]
+    conversation_id = event["pathParameters"]["conversationid"]
+    user = event["requestContext"]["authorizer"]["claims"]["sub"]
+
+    embeddings = get_embeddings()
+    faiss_index = get_faiss_index(embeddings, user, file_name)
+    memory = create_memory(conversation_id)
+    bedrock_runtime = boto3.client(
+        service_name="bedrock-runtime",
+        region_name="us-east-1",
+    )
+
+    handler = model_handlers.get(MODEL_ID)
+    if handler:
+        response = handler(faiss_index, memory, human_input, bedrock_runtime)
+    else:
+        raise ValueError(f"Unsupported MODEL_ID: {MODEL_ID}")
+
+    logger.info(str(response))
 
     return {
         "statusCode": 200,
@@ -74,5 +129,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
         },
-        "body": json.dumps(res["answer"]),
+        "body": json.dumps(response),
     }
